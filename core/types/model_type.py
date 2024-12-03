894e58b401d2d3615461f7866e93e2ca0045342d
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-12-03 14:57:27 +0600

    dev: :recycle: refactoring types

--- core/types/model_type.py
+++ core/types/model_type.py
@@ -1,8 +1,9 @@
-from typing import MutableMapping, TypeVar
+from typing import Any, MutableMapping, TypeVar, Union
 
 from django.db.models import Model
 
 __all__ = ["_T", "SerializerValidatedData"]
 
 _T = TypeVar("_T", bound=Model)
-SerializerValidatedData = MutableMapping
+
+SerializerValidatedData = MutableMapping[str, Union[str, int, Any]]

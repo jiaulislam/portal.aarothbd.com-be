678b73f4b009c4a906484c98d623ec5a6f46c678
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-12-07 20:12:47 +0600

    :dev :recycle: refactoring gunicorn.py to use defaults of gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -3,15 +3,6 @@ import multiprocessing
 # System Configs
 bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1 - 2
-worker_connections = 200
-max_requests = 1000
-max_requests_jitter = 1000
-graceful_timeout = 30
-limit_request_line = 4094
-reload = True
-proxy_allow_ips = "127.0.0.1"
-timeout = 30
-keepalive = 3
 
 # Logging
 accesslog = "./logs/gunicorn.access.log"

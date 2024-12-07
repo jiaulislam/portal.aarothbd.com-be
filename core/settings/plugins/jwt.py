ea8979f82dda4c5156ebd687fd26fc2290edd84c
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-12-07 23:01:25 +0600

    dev: :recycle: refactoring jwt access token & refresh token time

--- core/settings/plugins/jwt.py
+++ core/settings/plugins/jwt.py
@@ -3,8 +3,8 @@ from datetime import timedelta
 from ..django.base import env
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": True,

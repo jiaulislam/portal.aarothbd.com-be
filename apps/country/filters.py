58aefc8e18252922fa5036d2de36fb9e727fcaa4
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-12-12 16:48:36 +0600

    dev: adding ordering

--- apps/country/filters.py
+++ apps/country/filters.py
@@ -1,9 +1,23 @@
 from core.filter import BaseFilter
 
 from .models import Country
+from django_filters import OrderingFilter
 
 
 class CountryFilter(BaseFilter):
+    order_by = OrderingFilter(
+        fields=(
+            ("name", "name"),
+            ("country_code", "country_code"),
+            ("continent_code", "continent_code"),
+        ),
+        field_labels={
+            "name": "Name",
+            "country_code": "Country Code",
+            "continent_code": "Continent Code",
+        },
+    )
+
     class Meta:
         model = Country
         exclude = ["created_at", "updated_at", "created_by", "updated_by"]

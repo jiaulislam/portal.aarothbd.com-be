9c5cec61c84b22ffe1fe9041a9f3d93579763f6b
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-12-14 12:04:17 +0600

    dev: :construction: company work construction

--- apps/company/urls/urls_v1.py
+++ apps/company/urls/urls_v1.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from ..views.company_address_view_v1 import CompanyAddressCreateAPIView
+from ..views.company_address_view_v1 import CompanyAddressListCreateAPIView
+from ..views.company_configuration_view_v1 import CompanyConfigurationUpdateAPIView
 from ..views.company_view_v1 import (
     CompanyListCreateAPIView,
     CompanyRetrieveUpdateAPIView,
@@ -10,6 +11,19 @@ from ..views.company_view_v1 import (
 urlpatterns = [
     path(r"companies/", CompanyListCreateAPIView.as_view(), name="companies-list-create"),
     path(r"companies/<int:id>/", CompanyRetrieveUpdateAPIView.as_view(), name="company-retrieve-update"),
-    path(r"companies/<int:id>/update-status/", CompanyUpdateStatusAPIView.as_view(), name="company-update-status"),
-    path(r"companies/<int:company_id>/address/", CompanyAddressCreateAPIView.as_view(), name="company-address-create"),
+    path(
+        r"companies/<int:id>/update-status/",
+        CompanyUpdateStatusAPIView.as_view(),
+        name="company-update-status",
+    ),
+    path(
+        r"companies/<int:company_id>/addresses/",
+        CompanyAddressListCreateAPIView.as_view(),
+        name="company-addresses-list-create",
+    ),
+    path(
+        r"companies/<int:company_id>/configuration/",
+        CompanyConfigurationUpdateAPIView.as_view(),
+        name="company-configuration-update",
+    ),
 ]

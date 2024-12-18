dcf0311794a37d2c2e1a8c027bd89b7adf8fa37f
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-12-18 17:22:22 +0600

    refactor(product): refactoring product module

--- apps/product/views/product_brand_view_v1.py
+++ apps/product/views/product_brand_view_v1.py
@@ -1,5 +1,6 @@
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
+from django.db.models.query import QuerySet
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import DjangoModelPermissions
@@ -10,11 +11,17 @@ from core.request import Request
 from ..serializers.product_brand_serializer import ProductBrandSerializer
 from ..services.product_brand_service import ProductBrandService
 
+if TYPE_CHECKING:
+    from apps.product.models.product_brand_model import ProductBrand
+
 
 class ProductBrandListCreateAPIView(ListCreateAPIView):
     serializer_class = ProductBrandSerializer
     permission_classes = [DjangoModelPermissions]
     brand_service = ProductBrandService()
+
+    def get_queryset(self) -> QuerySet["ProductBrand"]:
+        return self.brand_service.all()
 
     def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         serializer = ProductBrandSerializer(data=request.data)

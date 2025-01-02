7808e0f87cdd4f02297743e73a5ec825a03bb206
jiaulislam <jiaulislam.ict.bd@gmail.com>
2025-01-02 23:22:10 +0600

    feat(pagination): working on pagination

--- core/pagination.py
+++ core/pagination.py
@@ -2,5 +2,5 @@ from rest_framework.pagination import LimitOffsetPagination
 
 
 class ExtendedLimitOffsetPagination(LimitOffsetPagination):
-    max_limit = 99
-    default_limit = 10
+    max_limit = 1000
+    default_limit = 50

60cbb00ca10ceb7f909abc05d37852a7bcb4f110
jiaulislam <jiaulislam.ict.bd@gmail.com>
2025-03-07 23:01:02 +0600

    fix(produc-image): fixing product image upload api view

--- apps/product/views/product_image_view_v1.py
+++ apps/product/views/product_image_view_v1.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 
 from ..models import ProductImage
@@ -8,6 +9,7 @@ from ..serializers import ProductImageSerializer
 class ProductImageCreateAPIView(CreateAPIView):
     serializer_class = ProductImageSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
 
     def get_queryset(self):
         return ProductImage.objects.all()

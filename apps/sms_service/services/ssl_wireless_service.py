8411a0f04f3a1e7f78e2f7ee7efebfdad94789b0
jiaulislam <jiaulislam.ict.bd@gmail.com>
2025-03-16 23:31:00 +0600

    refactor(ssl): :recycle: refactoring ssl csms id

--- apps/sms_service/services/ssl_wireless_service.py
+++ apps/sms_service/services/ssl_wireless_service.py
@@ -60,7 +60,7 @@ class SSLWirelessService:
             "sid": self.sid,
             "msisdn": f"88{phone_number}",
             "sms": message,
-            "csms_id": str(uuid4().hex),
+            "csms_id": str(uuid4().hex)[:20],
         }
         return data
 

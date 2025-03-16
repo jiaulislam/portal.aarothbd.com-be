8bad661d051fdff2be302c2e5fb834249f569fb3
jiaulislam <jiaulislam.ict.bd@gmail.com>
2025-03-16 23:50:14 +0600

    refactor(ssl): :recycle: refactoring ssl csms id

--- apps/testimonial/admin.py
+++ apps/testimonial/admin.py
@@ -1,1 +1,19 @@
-# Register your models here.
+from django.contrib import admin
+
+from core.admin import BaseAdmin
+from core.constants import AUDIT_COLUMNS
+
+from .models import Testimonial
+
+
+@admin.register(Testimonial)
+class TestimonialAdmin(BaseAdmin):
+    list_display = (
+        "id",
+        "client_name",
+        "client_position",
+        "is_active",
+    )
+    list_filter = ("is_active",)
+    readonly_fields = AUDIT_COLUMNS
+    list_editable = ("is_active",)

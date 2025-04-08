0c5574cf2c0598b06fb553cd96d65c2825568c61
jiaulislam <jiaulislam.ict.bd@gmail.com>
2025-04-08 22:42:33 +0600

    :feat: adding new public offers view

--- apps/offer/urls.py
+++ apps/offer/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views.cupon_view_v1 import CuponValidateAPIView
 from .views.offer_views_v1 import (
+    EcommerceOfferListAPIView,
     OfferAgreementAPIView,
     OfferListCreateAPIView,
     OfferRetrieveUpdateAPIView,
@@ -10,6 +11,7 @@ from .views.offer_views_v1 import (
 
 urlpatterns = [
     path(r"offers/", OfferListCreateAPIView.as_view(), name="offer-list-create-view"),
+    path(r"ecomm/offers/", EcommerceOfferListAPIView.as_view(), name="ecomm-offer-list-create-view"),
     path(r"offers/<str:slug>/", OfferRetrieveUpdateAPIView.as_view(), name="offer-retrieve-update-view"),
     path(r"offers/<str:id>/update-status/", OfferUpdateStatusAPIView.as_view(), name="offer-update-status-view"),
     path(r"offers/<str:slug>/update-agreement/", OfferAgreementAPIView.as_view(), name="offer-update-agreement-view"),

27ce481890f19fc9742d6e8730f241bcf5dcbe29
jiaulislam <jiaulislam.ict.bd@gmail.com>
2025-05-15 22:42:28 +0600

    :refactor: update import paths and improve URL patterns for clarity

--- apps/customer_order/urls.py
+++ apps/customer_order/urls.py
@@ -11,11 +11,27 @@ from .views.order_views_v1 import (
 )
 
 urlpatterns = [
-    path(r"orders/<int:order_id>/payments/", OrderPaymentListCreateAPIView.as_view(), name="order-payment-list-create"),
-    path(r"payments/<int:id>/reversal/", OrderPaymentReversalAPIView.as_view(), name="order-payment-reversal"),
+    path(
+        r"orders/<int:order_id>/payments/",
+        OrderPaymentListCreateAPIView.as_view(),
+        name="order-payment-list-create",
+    ),
+    path(
+        r"payments/<int:id>/reversal/",
+        OrderPaymentReversalAPIView.as_view(),
+        name="order-payment-reversal",
+    ),
     path(r"payments/", OrderPaymentListAPIView.as_view(), name="order-payment-list"),
-    path(r"payments/<int:id>/", OrderPaymentUpdateAPIView.as_view(), name="order-payment-update"),
+    path(
+        r"payments/<int:id>/",
+        OrderPaymentUpdateAPIView.as_view(),
+        name="order-payment-update",
+    ),
     path(r"orders/", OrderListCreateAPIView.as_view(), name="order-list-create"),
     path(r"orders/<int:id>/", OrderRetrieveUpdateAPIView.as_view(), name="order-retrieve"),
-    path(r"orders/<int:id>/update-status/", OrderUpdateStatusAPIView.as_view(), name="order-update-status"),
+    path(
+        r"orders/<int:id>/update-status/",
+        OrderUpdateStatusAPIView.as_view(),
+        name="order-update-status",
+    ),
 ]

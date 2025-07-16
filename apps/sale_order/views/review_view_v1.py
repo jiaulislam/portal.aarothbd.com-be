45011d7cf725e7cc66e97c95ff850826b3717e3b
jiaulislam <jiaulislam.ict.bd@gmail.com>
2025-07-16 22:56:36 +0600

    :fix: update conflicting URL for product reviews and remove unused permission in ReviewCreateAPIView

--- apps/sale_order/views/review_view_v1.py
+++ apps/sale_order/views/review_view_v1.py
@@ -1,5 +1,4 @@
 from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
 
 from ..models import Review
 from ..serializers import ReviewSerializer
@@ -8,7 +7,7 @@ __all__ = ["ReviewCreateAPIView"]
 
 
 # TODO: Need to implement some kind of rate limiting here
+# TODO: Need to move the review works to the product module
 class ReviewCreateAPIView(CreateAPIView):
     serializer_class = ReviewSerializer
     queryset = Review.objects.all()
-    permission_classes = [AllowAny]

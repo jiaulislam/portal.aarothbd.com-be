00b30d3fd37ceac59b0d77d44d695ca1cfe4e0bc
jiaulislam <jiaulislam.ict.bd@gmail.com>
2025-08-05 23:43:47 +0600

    refactor: update inline class definitions for consistency and clarity

--- apps/company/admin.py
+++ apps/company/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.contrib.contenttypes.admin import GenericStackedInline
-from unfold.contrib.inlines.admin import NonrelatedTabularInline
+from unfold.admin import TabularInline
+from unfold.contrib.inlines.admin import NonrelatedStackedInline
 
 from apps.address.models import Address
 from core.admin import BaseAdmin, InlineHelperAdmin
@@ -9,7 +9,7 @@ from core.constants import AUDIT_COLUMNS
 from .models import Company, CompanyCategory, CompanyConfiguration
 
 
-class CompanySettingsInline(admin.TabularInline, InlineHelperAdmin):
+class CompanySettingsInline(TabularInline, InlineHelperAdmin):
     model = CompanyConfiguration
     exclude = AUDIT_COLUMNS
     can_delete = False
@@ -18,13 +18,25 @@ class CompanySettingsInline(admin.TabularInline, InlineHelperAdmin):
     verbose_name = "Company Settings"
 
 
-class AddressInline(GenericStackedInline, NonrelatedTabularInline, InlineHelperAdmin):
+class AddressInline(NonrelatedStackedInline, InlineHelperAdmin):
     extra = 0
     model = Address
     fk_name = "addresses"
     ct_field = "content_type"
     ct_fk_field = "object_id"
     exclude = AUDIT_COLUMNS
+    fields = ("line_1", "line_2", "sub_district", "district", "division", "country", "address_type", "is_default")
+
+    def get_form_queryset(self, obj):
+        queryset = self.model.objects.filter(content_type__model="company", object_id=obj.pk)
+        return queryset
+
+    def save_new_instance(self, parent, instance):
+        """
+        Extra save method which can for example update inline instances based on current
+        main model object. Method must be implemented.
+        """
+        pass
 
 
 @admin.register(CompanyCategory)
@@ -41,7 +53,7 @@ class CompanyAdmin(BaseAdmin):
     list_display = ("name", "slug", "theme_color", "is_active")
     search_fields = ("name", "slug", "tin_number", "bin_number")
     list_filter = ("is_active",)
-    inlines = (CompanySettingsInline, AddressInline)
+    inlines = [CompanySettingsInline, AddressInline]
     readonly_fields = AUDIT_COLUMNS
     filter_horizontal = ("allowed_products",)
     list_editable = ("is_active",)

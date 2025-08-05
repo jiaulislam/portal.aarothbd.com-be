b3b05612e65a450263b4483f9caccb5179e80031
jiaulislam <jiaulislam.ict.bd@gmail.com>
2025-08-05 23:59:53 +0600

    feat: implement bulk creation of order lines in PurchaseOrderListCreateView

--- apps/purchase_order/views.py
+++ apps/purchase_order/views.py
@@ -28,7 +28,18 @@ class PurchaseOrderListCreateView(ListCreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        order_lines_data = serializer.validated_data.pop("order_lines", [])
         purchase_order = serializer.save()
+
+        # Bulk create order lines
+        if order_lines_data:
+            from .models import PurchaseOrderLine  # Assuming this model exists
+
+            order_lines = [
+                PurchaseOrderLine(purchase_order=purchase_order, **line_data) for line_data in order_lines_data
+            ]
+            PurchaseOrderLine.objects.bulk_create(order_lines)
+
         response = PurchaseOrderSerializer(purchase_order).data
         return Response(response, status=status.HTTP_201_CREATED)
 
